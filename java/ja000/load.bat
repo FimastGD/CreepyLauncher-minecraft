fff58bae2d6aa8d917cdcf6dd3e4806f315a0bd7
FimastGD <113923761+FimastGD@users.noreply.github.com>
2023-08-15 13:39:36 +0300

    Uploaded Alpha 0.0.0

---
+++ java/ja000/load.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C:\creepytoast\java\ja000
+color 0f
+java -Xmx1024M -Xms1024M -cp Minecraft.jar "-Dorg.lwjgl.librarypath=%CD%/natives" "-Dnet.java.games.input.librarypath=%CD%/natives" Start
+pause
